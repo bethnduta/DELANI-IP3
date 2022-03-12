c8e55809526d75a5c6288067f07dcd871ad367b0
beth nduta <bethnduta05@gmail.com>
2022-03-12 15:26:46 +0300

    developed the form using the JQUERY

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ $(document).ready(function () {
         $(".design").toggle();
     })
 })
+
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        let userName = $("input#name").val();
+        let userMail = $("input#email").val();
+        let userMessage = $("input#message").val();
+        if (userName && userMail && userMail != "") {
+            alert(`Hi ${userName} we have received your message, Thank you for reaching out to us.`)
+        }
+        event.preventDefault();
+    })
+})
