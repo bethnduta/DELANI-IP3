906f168ec029c78da50ee51b162a8218c66df822
beth nduta <bethnduta05@gmail.com>
2022-03-12 15:18:24 +0300

    updated my app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello');
+$(document).ready(function () {
+    $(".img1").click(function () {
+        $(".pdesign").toggle();
+        $(".design").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".img1").click(function () {
+        $(".pdesign").toggle();
+        $(".design").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".img1").click(function () {
+        $(".pdesign").toggle();
+        $(".design").toggle();
+    })
+})
